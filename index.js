1ad7582485f581a6f1e33aedf2c9fecbc4890828
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-04-12 16:27:35 +0200

    IT1 done

--- index.js
+++ index.js
@@ -1,110 +1,116 @@
 // ***************************************************************************
 // Iteration 1 - `for...of` loop
 // ***************************************************************************
+let usersArray = require('./data.js');
 
-const getFirstNames = arr => {
+function getFirstNames(arr){
   const userFirstNames = [];
   for (let user of arr) {
-    // Your code goes here ...
+    userFirstNames.push(user.firstName)
+    
   }
+  console.log(userFirstNames)
+  return userFirstNames
+  
 };
+
 
 getFirstNames(usersArray);
-// expected output:
-// [ 'Kirby', 'Tracie', 'Kendra', 'Kinney', 'Howard', 'Rachelle', 'Lizzie' ]
+// // expected output:
+// // [ 'Kirby', 'Tracie', 'Kendra', 'Kinney', 'Howard', 'Rachelle', 'Lizzie' ]
 
-// ***************************************************************************
-// Iteration 2 - `for...of` loop and ES6 string literals `${}`
-// ***************************************************************************
+// // ***************************************************************************
+// // Iteration 2 - `for...of` loop and ES6 string literals `${}`
+// // ***************************************************************************
 
-const getFullNames = arr => {
-  // Your code goes here ...
-};
+// const getFullNames = arr => {
+//   // Your code goes here ...
+// };
 
-getFullNames(usersArray);
-// expected output:
-// [ 'Kirby Doyle', 'Tracie May', 'Kendra Hines', 'Kinney Howard',
-//   'Howard Gilmore', 'Rachelle Schneider', 'Lizzie Alford' ]
+// getFullNames(usersArray);
+// // expected output:
+// // [ 'Kirby Doyle', 'Tracie May', 'Kendra Hines', 'Kinney Howard',
+// //   'Howard Gilmore', 'Rachelle Schneider', 'Lizzie Alford' ]
 
-// ***************************************************************************
-// Iteration 3 - ES6 destructuring , for of loop, object literal
-// ***************************************************************************
+// // ***************************************************************************
+// // Iteration 3 - ES6 destructuring , for of loop, object literal
+// // ***************************************************************************
 
-const getUsersCreditDetails = arr => {
-  // Your code goes here ...
-};
+// const getUsersCreditDetails = arr => {
+//   // Your code goes here ...
+// };
 
-getUsersCreditDetails(usersArray);
-// expected output:
-// [ { firstName: 'Kirby', lastName: 'Doyle', balance: '$3,570.06' },
-// { firstName: 'Tracie', lastName: 'May', balance: '$1,547.73' },
-// { firstName: 'Kendra', lastName: 'Hines', balance: '$12,383.08' },
-// { firstName: 'Kinney', lastName: 'Howard', balance: '$3,207.06' },
-// { firstName: 'Howard', lastName: 'Gilmore', balance: '$21,307.75' },
-// { firstName: 'Rachelle', lastName: 'Schneider', balance: '$35,121.49' },
-// { firstName: 'Lizzie', lastName: 'Alford', balance: '$4,382.94' } ]
+// getUsersCreditDetails(usersArray);
+// // expected output:
+// // [ { firstName: 'Kirby', lastName: 'Doyle', balance: '$3,570.06' },
+// // { firstName: 'Tracie', lastName: 'May', balance: '$1,547.73' },
+// // { firstName: 'Kendra', lastName: 'Hines', balance: '$12,383.08' },
+// // { firstName: 'Kinney', lastName: 'Howard', balance: '$3,207.06' },
+// // { firstName: 'Howard', lastName: 'Gilmore', balance: '$21,307.75' },
+// // { firstName: 'Rachelle', lastName: 'Schneider', balance: '$35,121.49' },
+// // { firstName: 'Lizzie', lastName: 'Alford', balance: '$4,382.94' } ]
 
-// ***************************************************************************
-// Iteration 4 - practice `.filter()` method and how to return two elements
-// ***************************************************************************
+// // ***************************************************************************
+// // Iteration 4 - practice `.filter()` method and how to return two elements
+// // ***************************************************************************
 
-const genderView = users => {
-  // Your code goes here ...
-};
+// const genderView = users => {
+//   // Your code goes here ...
+// };
 
-genderView(usersArray);
-// expected output:
-// {
-//    femaleUsers: [ 'Tracie May', 'Kendra Hines', 'Rachelle Schneider', 'Lizzie Alford' ],
-//    maleUsers: [ 'Kirby Doyle', 'Kinney Howard', 'Howard Gilmore' ]
-// }
+// genderView(usersArray);
+// // expected output:
+// // {
+// //    femaleUsers: [ 'Tracie May', 'Kendra Hines', 'Rachelle Schneider', 'Lizzie Alford' ],
+// //    maleUsers: [ 'Kirby Doyle', 'Kinney Howard', 'Howard Gilmore' ]
+// // }
 
-// ***************************************************************************
-// Bonus - Iteration 5
-// ***************************************************************************
+// // ***************************************************************************
+// // Bonus - Iteration 5
+// // ***************************************************************************
 
-const data = genderView(usersArray);
+// const data = genderView(usersArray);
 
-const genderCount = data => {
-  // Your code goes here ...
-};
+// const genderCount = data => {
+//   // Your code goes here ...
+// };
 
-genderCount(data);
-// expected output:
-// Female: 4
-// Male: 3
+// genderCount(data);
+// // expected output:
+// // Female: 4
+// // Male: 3
 
-// ***************************************************************************
-// Bonus - Iteration 6
-// ***************************************************************************
+// // ***************************************************************************
+// // Bonus - Iteration 6
+// // ***************************************************************************
 
-const promo20 = users => {
-  // Your code goes here ...
-};
+// const promo20 = users => {
+//   // Your code goes here ...
+// };
 
-// expected output:
-// Dear Howard, since your balance is $21,307.75, you are eligible to apply for this awesome credit card.
-// Dear Rachelle, since your balance is $35,121.49, you are eligible to apply for this awesome credit card.
+// // expected output:
+// // Dear Howard, since your balance is $21,307.75, you are eligible to apply for this awesome credit card.
+// // Dear Rachelle, since your balance is $35,121.49, you are eligible to apply for this awesome credit card.
 
-// ***************************************************************************
-// Bonus - Iteration 7
-// ***************************************************************************
+// // ***************************************************************************
+// // Bonus - Iteration 7
+// // ***************************************************************************
 
-const addActive = users => {
-  // Your code goes here ...
-};
+// const addActive = users => {
+//   // Your code goes here ...
+// };
 
-addActive(usersArray);
-// expected output:
-// [
-//    { firstName: 'Kirby',
-//      lastName: 'Doyle',
-//      id: 'b71794e5-851e-44b5-9eec-1dd4e897e3b8',
-//      isActive: true,
-//      balance: '$3,570.06',
-//      gender: 'male'
-//    },
-//    {
-//      // ...
-//    }
-// ]
+// addActive(usersArray);
+// // expected output:
+// // [
+// //    { firstName: 'Kirby',
+// //      lastName: 'Doyle',
+// //      id: 'b71794e5-851e-44b5-9eec-1dd4e897e3b8',
+// //      isActive: true,
+// //      balance: '$3,570.06',
+// //      gender: 'male'
+// //    },
+// //    {
+// //      // ...
+// //    }
+// // ]
